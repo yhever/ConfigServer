045c7b79a665d9aec34cb2ff870c34db3649fd0d
calvin <calvin.ye@capgemini.com>
2017-12-25 14:41:05 +0800

    update

---
+++ ConfigServer/src/main/java/com/yhever/configserver/App.java
@@ -0,0 +1,13 @@
+package com.yhever.configserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

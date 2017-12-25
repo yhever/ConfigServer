65aa9411bef1f9ee2138167654213a81deb6b81b
calvin <calvin.ye@capgemini.com>
2017-12-25 17:38:01 +0800

    initial code

---
+++ ConfigServer/src/main/java/com/yhever/configserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.yhever.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
